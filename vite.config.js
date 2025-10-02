98d874081f54973e18175cd9bd4aba13ac6d2f1a
Ryan Helou <ryan.helou@gmail.com>
2025-10-02 15:28:13 -0400

    Update package.json

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/<todo-list>/',
+  // must match your repo name exactly (case-sensitive)
+  base: '/todo-list/',
+  // this makes the production build go into /docs
+  build: { outDir: 'docs' },
 })
