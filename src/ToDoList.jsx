4eec5c19b186937102c1cb009983ecad33da801a
Ryan Helou <ryan.helou@gmail.com>
2025-10-02 16:51:55 -0400

    formatting + delete button

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,45 +1,48 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-function TodoList(){
-    
-    const[tasks,setTasks] = useState(["Make a million", "Be a baller", "Make a billion"]);
-    const[newTask, setNewTask] = useState("");
+function TodoList() {
+  const [tasks, setTasks] = useState([
+    "Make a million",
+    "Be a baller",
+    "Make a billion",
+  ]);
+  const [newTask, setNewTask] = useState("");
 
-    function handleInputChange(event){
-        setNewTask(event.target.value);
-    }
-    
-    function addTask(){
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
 
-    }
-    
-    function deleteTask(index){
+  function addTask() {}
 
-    }
-    
-    return (
-        <div className = "to-do-list">
+  function deleteTask(index) {}
 
-            <h1>To Do List!</h1>
+  return (
+    <div className="to-do-list">
+      <h1>To Do List!</h1>
 
-            <div>
-                <input
-                    type = "text"
-                    placeholder = "Enter task"
-                    value = {newTask}
-                    onChange = {handleInputChange}
-                >
-                </input>
-                <button
-                    className = "button-add"
-                    onClick = {addTask}
-                    >
-                    Add
-                </button>
-            </div>
-
-        </div>
-    )
-
+      <div>
+        <input
+          type="text"
+          placeholder="Enter task"
+          value={newTask}
+          onChange={handleInputChange}
+        ></input>
+        <button className="button-add" onClick={addTask}>
+          Add
+        </button>
+      </div>
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <span className="text">{task}</span>
+            <button className="delete-button" onClick={() => deleteTask(index)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
-export default TodoList
+export default TodoList;
