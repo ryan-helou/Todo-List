f8fe51847ae0cccc180161aa5b5342023ac209c4
Ryan Helou <ryan.helou@gmail.com>
2025-10-02 14:22:24 -0400

    Update ToDoList.jsx

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -4,8 +4,35 @@ function TodoList(){
     
     const[tasks,setTasks] = useState([]);
     const[newTask, setNewTask] = useState("");
+
+    function handleInputChange(event){
+
+    }
     
-    return (<></>)
+    function addTask(){
+
+    }
+    
+    function deleteTask(index){
+
+    }
+    
+    return (
+        <div className = "to-do-list">
+
+            <h1>To Do List!</h1>
+
+            <div>
+                <input>
+                    type = "text"
+                    placeholder = "Enter a task"
+                    value = {newTask}
+                    
+                </input>
+            </div>
+
+        </div>
+    )
 
 }
 export default TodoList
