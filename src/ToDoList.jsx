87b9106eaa3742ecd0994655445c78d18380c384
Ryan Helou <ryan.helou@gmail.com>
2025-10-02 14:18:32 -0400

    Update ToDoList.jsx

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,5 +1,10 @@
 import React, {useState} from 'react'
+
 function TodoList(){
+    
+    const[tasks,setTasks] = useState([]);
+    const[newTask, setNewTask] = useState("");
+    
     return (<></>)
 
 }
